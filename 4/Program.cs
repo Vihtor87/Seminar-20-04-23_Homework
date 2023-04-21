7c6c8cbee6d422ca257672fee2901708fd243bc4
Viktor Vinogradov <viktor.vinogradov@list.ru>
2023-04-21 10:42:02 +0300

    Add program №4

---
+++ 4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите прошрамму, котторая на входе принимает число, а на выходе выдаёт все числа от -N до N
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+int start = -number;
+while (start <= number)
+{
+    Console.Write(start + " ");
+    start++;
+}
