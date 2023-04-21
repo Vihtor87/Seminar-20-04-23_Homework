f735fae87a040a9eb2aac47cfa8178172fa068c1
Viktor Vinogradov <viktor.vinogradov@list.ru>
2023-04-21 10:12:13 +0300

    Change program №2

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,8 +1,8 @@
 ﻿// Определить является первое число квадратом второго
 Console.WriteLine("Ввведите число: ");
-int a = int.Parse(Console.ReadLine());
+int a = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите второе число: ");
-int b = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine()!);
 if (b * b == a)
     Console.WriteLine("Yes");
 else
