64bb61511163eee53da54445a386b47f1395e83d
Viktor Vinogradov <viktor.vinogradov@list.ru>
2023-04-21 22:59:26 +0300

    Add my homework №2

---
+++ Homework/Max_1or2or3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти наименьшее из трёх введённых чисел
+
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите первое число: ");
+int num3 = int.Parse(Console.ReadLine()!);
+int maxnum = num1;
+if (num2 > num3 & num2 > maxnum)
+{
+    maxnum = num2;
+}
+else if (num3 > num1 & num3 > maxnum)
+{
+    maxnum = num3;
+}
+Console.Write("Наибольшее число: " + maxnum);
