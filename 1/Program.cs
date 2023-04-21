09bd67c5a4871bcbae2e81d4c5f27f06e200be00
Viktor Vinogradov <viktor.vinogradov@list.ru>
2023-04-21 10:12:02 +0300

    Change program №1

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Вычислить квадрат введённого числа
 
 Console.WriteLine("Введите число: ");
-int number = int.Parse(Console.ReadLine());
+int number = int.Parse(Console.ReadLine()!);
 Console.WriteLine ("Квадрат введённого числа равен: " + number * number);
