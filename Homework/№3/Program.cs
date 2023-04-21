a2706d6df786492a5d13870fc5c8ae9de945f7ef
Viktor Vinogradov <viktor.vinogradov@list.ru>
2023-04-22 00:41:09 +0300

    Add program in Homework №3

---
+++ Homework/№3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Проверка на чётность
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+if (num %2 == 0)
+{
+    Console.WriteLine(num + " - чётное");
+}
+else
+{
+    Console.WriteLine(num + " - нечётное");
+}
