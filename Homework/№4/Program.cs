ee93195c7d8a3510d1af672f5449d9d7698baa3e
Viktor Vinogradov <viktor.vinogradov@list.ru>
2023-04-23 01:59:07 +0300

    Add Homework №4

--- Homework/№4/Program.cs
+++ Homework/№4/Program.cs
@@ -1,2 +1,11 @@
 ﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+int start = 1;
+while (start <= number)
+{
+     if (start % 2 == 0)
+     Console.Write(start + " ");
+     start++;
+}
