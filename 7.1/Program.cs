c6e62a9b6f6623b9cf61d1cc4b6b5a5bf1869f4b
Viktor Vinogradov <viktor.vinogradov@list.ru>
2023-04-26 01:30:34 +0300

    Add folder 7.1

---
+++ 7.1/Program.cs
@@ -0,0 +1,15 @@
+﻿Main(); // Программа, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+void Main()
+{
+    int rand = new Random().Next(100, 1000);
+    Console.WriteLine("Рандомное число: " + rand);
+    Console.WriteLine(FirstInNum(rand));
+}
+int FirstInNum(int num)
+{
+    int a = num / 100;
+    int b = num % 10;
+    int r = a * 10 + b;
+    return r;
+}
