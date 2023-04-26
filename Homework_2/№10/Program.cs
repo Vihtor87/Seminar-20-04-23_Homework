751864973e11a5ebc613967dc7f70e45be5325e2
Viktor Vinogradov <viktor.vinogradov@list.ru>
2023-04-26 12:43:11 +0300

    Add task № 13 in Homework_2

---
+++ Homework_2/№10/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Main(); // Программа, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа.
+
+void Main()
+{
+    Console.Write("Введите трёхзначное число: ");
+    int n = int.Parse(Console.ReadLine()!);
+    Console.Write(FindSecondInNum(n));
+
+    int FindSecondInNum(int num) // Берём вторую цифру из введённого числа
+    {
+        int x = num / 10 % 10;
+        return x;
+    }
+}
