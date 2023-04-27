d005b865c7605d64c1a83e3ec5f19260e2ac8d47
Viktor Vinogradov <viktor.vinogradov@list.ru>
2023-04-28 00:54:42 +0300

    Change task in folder №11

--- 11/Program.cs
+++ 11/Program.cs
@@ -3,22 +3,23 @@
 // A (3,6); B (2,1) -> 5,09 
 // A (7,-5); B (1,-1) -> 7,21
 // AB = √(xb - xa)2 + (yb - ya)2
-
-double x1 = Prompt("Введите координату X1: ");
-double y1 = Prompt("Введите координату Y1: ");
-double x2 = Prompt("Введите координату X2: ");
-double y2 = Prompt("Введите координату Y2: ");
+int x1 = Prompt("Введите координату X1: ");
+int y1 = Prompt("Введите координату Y1: ");
+int x2 = Prompt("Введите координату X2: ");
+int y2 = Prompt("Введите координату Y2: ");
 FindLinePlane(x1, y1, x2, y2);
 
-double Prompt(string message)
+int Prompt(string message)
 {
     Console.Write(message);
-    double coordNumber = double.Parse(Console.ReadLine()!);
-    return coordNumber;
+    int value = int.Parse(Console.ReadLine()!);
+    int coord = Convert.ToInt32(value);
+    return coord;
 }
 
-void FindLinePlane(double xa, double ya, double xb, double yb)
+double FindLinePlane(int xa, int ya, int xb, int yb)
 {
-    double ab = Math.Sqrt(Math.Pow((xb - xa), 2) + Math.Pow((yb - ya), 2));
+    double ab = Convert.ToDouble(Math.Round(Math.Sqrt(Math.Pow((xb - xa), 2) + Math.Pow((yb - ya), 2)), 3));
     Console.WriteLine($"Расстояние между точками {ab}");
+    return ab;
 }
