f49721bf1c32b66709c0a3737e2a274636dca707
Viktor Vinogradov <viktor.vinogradov@list.ru>
2023-05-20 17:03:17 +0300

    Add folder №14

---
+++ 14/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5
+*/
+
+int Prompt()
+{
+    Console.Write("Введите число: ");
+    int value = int.Parse(Console.ReadLine()!);
+    return value;
+}
+
+int QuantumNum(int x)
+{
+    int count = 0;
+    if (x != 0)
+    {
+        while (x > 0 || x < 0)
+        {
+            x /= 10;
+            count++;
+        }
+        return count;
+    }
+    return 1;
+}
+
+int num = Prompt();
+Console.Write($"В числе {num} количество цифр {QuantumNum(num)}");
