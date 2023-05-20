3df68fd65f6d11ec6df8a28b3656bf0fea55cca0
Viktor Vinogradov <viktor.vinogradov@list.ru>
2023-05-20 16:10:59 +0300

    Add folder №13

---
+++ 13/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36
+*/
+
+int n = Prompt();
+Console.Write($"Сумма чисел от 1 до {n} равна {GetSum(n)}");
+
+int Prompt()
+{
+    Console.Write("Введите число: ");
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+int GetSum(int x)
+{
+    int summ = 0;
+    for (int i = 1; i <= x; i++)
+    {
+        summ += i;
+    }
+    return summ;
+}
