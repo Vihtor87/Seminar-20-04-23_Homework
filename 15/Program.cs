c71e18985a4f8b389f829ebcb682cc190e01369f
Viktor Vinogradov <viktor.vinogradov@list.ru>
2023-05-20 16:11:15 +0300

    Add folder №15

---
+++ 15/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+4 -> 24
+5 -> 120
+*/
+
+int n = Prompt();
+Console.Write($"Произведение чисел от 1 до {n} равна {GetMult(n)}");
+
+int Prompt()
+{
+    Console.Write("Введите число: ");
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+int GetMult(int x)
+{
+    int multi = 1;
+    for (int i = 1; i <= x; i++)
+    {
+        multi *= i;
+    }
+    return multi;
+}
