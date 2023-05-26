ed5148f9f4ae98ff3fdc0f91942f4c75193934b4
Viktor Vinogradov <viktor.vinogradov@list.ru>
2023-05-26 15:25:06 +0300

    Add folder 24

---
+++ 24/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+45 -> 101101
+3 -> 11
+2 -> 10
+*/
+// int a = Prompt("Введите число: ");
+
+// int Prompt(string message)
+// {
+//     Console.Write(message);
+//     int value = int.Parse(Console.ReadLine()!);
+//     return value;
+// }
+
+int RandomNum()
+{
+    int rand = new Random().Next(100, 1000);
+    Console.WriteLine("Рандомное число: " + rand);
+    return rand;
+}
+int a = RandomNum();
+string ConvTo(int x)
+{
+    string binary = String.Empty;
+    while (x > 0)
+    {
+        binary = (x % 2).ToString() + binary;
+        x /= 2;
+    }
+    return binary;
+}
+Console.Write($"Число {a} в двоичном представлении /{ConvTo(a)}/");
