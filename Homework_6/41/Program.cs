f16bccbb16a84dcd1be384c462b3023d946a3f23
Viktor Vinogradov <viktor.vinogradov@list.ru>
2023-05-27 18:32:45 +0300

    Homework_6 it's DONE!

---
+++ Homework_6/41/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задача 41:
+Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+Console.Clear();
+
+string Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine()!;
+    return value;
+}
+
+void FindPositivNum(string words)
+{
+    string[] text = words.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+    int count = 0;
+    int N = 0;
+    foreach (string s in text)
+    {
+        N = Convert.ToInt32(s);
+        if (N > 0) count++;
+    }
+    Console.WriteLine($"Количество чисел больше нуля -> {count}.");
+}
+
+string num = Prompt("Введите числа через пробел: ");
+FindPositivNum(num);
---
+++ Homework_6/43/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 43:
+Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+double Prompt(string message)
+{
+    Console.Write(message);
+    double result = double.Parse(Console.ReadLine()!);
+    return result;
+}
+
+void FindPointIntersection(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.Write($"Точки пересечения заданных прямых ({x};{y})");
+}
+
+double a = Prompt("Введите значение: ");
+double b = Prompt("Введите значение: ");
+double c = Prompt("Введите значение: ");
+double d = Prompt("Введите значение: ");
+FindPointIntersection(a, b, c, d);
