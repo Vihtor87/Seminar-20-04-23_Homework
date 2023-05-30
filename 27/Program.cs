cebbd2260d8363bfbad28998122fd41f39e66254
Viktor Vinogradov <viktor.vinogradov@list.ru>
2023-05-30 09:32:48 +0300

    Add folder 27

---
+++ 27/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+m = 3, n = 4.
+1 4 8 19
+5 -2 33 -2
+77 3 8 1
+*/
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(rows, columns, 0, 10); // принимаем количество строк и столбцов и задаём интервал заполнения
+PrintArray(array);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue) // принимаем количество строк и столбцов
+{
+    int[,] res = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            res[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return res;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
