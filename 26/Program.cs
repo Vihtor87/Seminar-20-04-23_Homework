49edcf6b838d34b58b13ec3b7cfb62887cb78983
Viktor Vinogradov <viktor.vinogradov@list.ru>
2023-05-30 09:47:43 +0300

    Add folder 26

---
+++ 26/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+*/
+
+int[] arr1 = { 1, 2, 34, 4, 5 };
+Console.Write(String.Join(" ", arr1));
+Console.WriteLine();
+int[] arr2 = {2, 3, 827, 3, 3, 0};
+Console.Write(String.Join(" ", arr2));
+Console.WriteLine();
+
+CopyArray(arr1);
+Console.WriteLine();
+CopyArray(arr2);
+
+void CopyArray(int[] inArray)
+{
+    int[] res = new int[inArray.Length];
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        res[i] = inArray[i];
+    }
+    Console.Write(String.Join(" ", res));
+}
