0af0f90d23a7495e30fb0d3d68df73b91a4199e3
Viktor Vinogradov <viktor.vinogradov@list.ru>
2023-06-02 22:55:02 +0300

    Change task 21

--- 21/Program.cs
+++ 21/Program.cs
@@ -5,36 +5,29 @@
 [6 7 3 6] -> 36 21
 */
 
+int[] arr = GetArray(7, 1, 10);
+Console.WriteLine($"[{String.Join(" ", arr)}]");
+Console.WriteLine($"[{String.Join(" ", GetMult(arr))}]");
+
 int[] GetArray(int size, int minValue, int maxValue)
 {
     int[] res = new int[size];
     for (int i = 0; i < size; i++)
     {
         res[i] = new Random().Next(minValue, maxValue + 1);
-        Console.Write($"{res[i]} ");
     }
     return res;
 }
 
-void GetMult(int[] array)
+int[] GetMult(int[] array)
 {
-    int multi = 0;
-    int iFin = array.Length - 1;
-    for (int i = 0; i <= (iFin / 2); i++)
+    int size = array.Length / 2;
+    if (array.Length % 2 != 0) size++;
+    int[] multyArray = new int[size];
+    for (int i = 0; i < array.Length / 2; i++)
     {
-        if (array.Length % 2 == 0)
-        {
-            multi = array[i] * array[iFin - i];
-            Console.Write($"{multi} ");
-        }
-        else
-        {
-            multi = array[i] * array[iFin - i];
-            Console.Write($"{multi} ");
-        }
+        multyArray[i] = array[i] * array[array.Length - 1 - i];
     }
+    multyArray[size - 1] = array[array.Length / 2];
+    return multyArray;
 }
-
-int[] arr = GetArray(7, 1, 10);
-Console.WriteLine();
-GetMult(arr);
