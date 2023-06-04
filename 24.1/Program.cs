493e3f988e5d36bed400a9f0e1733a71f1e6ccf0
Viktor Vinogradov <viktor.vinogradov@list.ru>
2023-06-04 17:07:14 +0300

    Add folders 24.1 with another code

---
+++ 24.1/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+45 -> 101101
+3 -> 11
+2 -> 10
+*/
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+string value = Convert.ToString(number, 2);
+Console.WriteLine($"Число {number} в двоичной системе исчисления -> {value}");
